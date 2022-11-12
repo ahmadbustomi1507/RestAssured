4a90fdc69d712dcf061b5ef56f4b7f90c64e0311
tomi <adl.ahmadb@xl.co.id>
2022-11-12 15:47:17 +0700

    replace old project

---
+++ project_selenium/src/test/java/com/automation/my_test.java
@@ -0,0 +1,27 @@
+package com.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class my_test {
+
+    @BeforeTest
+    public void set_driver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium_Driver\\chromedriver.exe");
+    }
+
+    @Test
+    public void test_get_driver() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.getTitle();
+        driver.quit();
+    }
+}
